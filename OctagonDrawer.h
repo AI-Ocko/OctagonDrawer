0ba302210ec8d33fdee02d3d18e6d7e5b2bcda9a
christopher <135375609+AI-Ocko@users.noreply.github.com>
2025-04-08 21:56:12 -0700

    Added Private variables

---
+++ OctagonDrawer.h
@@ -0,0 +1,21 @@
+#ifndef _OCTAGON
+#define _OCTAGON 
+
+class OctagonDrawer {
+
+    private:
+        int side;
+        int spacesDrawn;
+        int starsDrawn;
+        int spacesOut;
+        int spacesIn;
+        int rowsDrawn;
+        int rowsToDraw;
+
+    public:
+
+
+
+};
+
+#endif // !_OCTAGON

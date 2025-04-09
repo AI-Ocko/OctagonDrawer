6881ea85ef2f6a8338b7ac1c1eec33ddc9c5216f
christopher <135375609+AI-Ocko@users.noreply.github.com>
2025-04-09 11:40:30 -0700

    defined multiple OctagonDrawer functions still need to define display function

--- OctagonDrawer.cpp
+++ OctagonDrawer.cpp
@@ -3,6 +3,36 @@
 
 using namespace std;
 
+OctagonDrawer::OctagonDrawer() {
+    side = 3;
+
+}
+
+OctagonDrawer::OctagonDrawer(const int sidePar) {
+    if (sidePar < 3 || sidePar > 8) {
+        cout << "The side value must be between 3 and 8" << endl;
+
+    }
+    else {
+        side = sidePar;
+    }
+}
+
+void OctagonDrawer::setSide(const int sidePar) {
+    if (sidePar < 3 || sidePar > 8) {
+        cout << "The side value must be between 3 and 8" << endl;
+
+    }
+    else {
+        side = sidePar;
+    }
+}
+
+int OctagonDrawer::getside() const {
+    return side;
+}
+
+
 int main () {
 
     return 0;

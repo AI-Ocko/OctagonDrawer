a437359d55d72ac9e63135b541be8a4591e6877d
christopher <135375609+AI-Ocko@users.noreply.github.com>
2025-04-13 20:56:14 -0700

    Added display Octagon function

--- OctagonDrawer.cpp
+++ OctagonDrawer.cpp
@@ -33,6 +33,132 @@ int OctagonDrawer::getside() const {
 }
 
 void OctagonDrawer::display(const int sidePar) {
+    if (sidePar < 3 || sidePar > 8) {
+        cout << "THe side value must be between 3 and 8" << endl;
+    }
+    else {
+        string output = ""; //clear the output
+
+        spacesDrawn = 0;
+        starsDrawn = 0;
+        spacesOut = side - 1;
+        spacesIn = 0;
+        rowsToDraw = 2 * side + (side - 2);
+
+        while (rowsDrawn < rowsToDraw)
+        {
+            if (rowsDrawn == 0)
+            {
+                spacesDrawn = 0;
+                //draw spaces outside first
+                while (spacesDrawn < spacesOut) 
+                {
+                    output = output + " ";
+                    spacesDrawn ++;
+                }
+
+                starsDrawn = 0;
+                
+                //draw stars on the row
+                while (starsDrawn < side)
+                {
+                    output = output + "*";
+                    starsDrawn ++;
+                }
+                //get ready for the next iteration
+                spacesOut --;
+                spacesIn = side;
+                rowsDrawn ++;
+                output = output + "\n";
+            }
+
+            else if (rowsDrawn < side)
+            {
+                spacesDrawn = 0;
+
+                while (spacesDrawn < spacesOut) {
+                    output = output + " ";
+                    spacesDrawn ++;
+                }
+
+                output = output + "*";
+                spacesDrawn = 0;
+
+                while (spacesDrawn < spacesIn)
+                {
+                    output = output + " ";
+                    spacesDrawn ++;
+                }
+                
+                output = output + "*";
+                spacesOut --;
+                spacesIn += 2;
+                rowsDrawn ++;
+                output = output + "\n";
+            }
+
+            else if (rowsDrawn < (2 * side -1)) 
+            {
+                output = output + "*";
+                spacesDrawn = 0;
+
+                while (spacesDrawn < 2 * side + (side -4))
+                {
+                    output = output + " ";
+                    spacesDrawn ++;
+                }
+
+                output = output + "*";
+                spacesOut = 1;
+                rowsDrawn ++;
+                output = output + "\n";
+                
+            }            
+            
+            else if (rowsDrawn < 2 * side + (side - 3))
+            {
+                spacesDrawn = 0;
+                while (spacesDrawn < spacesOut)
+                {
+                    output = output + " ";
+                    spacesDrawn ++;
+                }
+                output = output + "*";
+                spacesDrawn = 0;
+                while (spacesDrawn < spacesIn - 4)
+                {
+                    output = output + " ";
+                    spacesDrawn++;
+                }
+                output = output + "*";
+                spacesOut ++;
+                spacesIn -= 2;
+                rowsDrawn ++;
+                output = output + "\n";
+                
+            }
+
+            else if (rowsDrawn < 2 * side + (side - 2))
+            {
+                spacesDrawn = 0;
+                while (spacesDrawn < spacesOut)
+                {
+                    output = output + " ";
+                    spacesDrawn ++;
+                }
+                starsDrawn = 0;
+                while (starsDrawn < side)
+                {
+                    output = output + "*";
+                    starsDrawn ++;
+                }
+                rowsDrawn ++;
+                
+            }
+            
+        }   
+
+    }
 
 }
 
